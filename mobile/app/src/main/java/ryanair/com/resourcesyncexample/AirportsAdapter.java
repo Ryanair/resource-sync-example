895d0a5a8eebd6ed38be566fe4186bbccdac8fa4
Vlado Atanasov <vlado.atanasov@gmail.com>
2015-05-14 22:13:40 +0100

    implemented recyclerview with the couchbase results

---
+++ mobile/app/src/main/java/ryanair/com/resourcesyncexample/AirportsAdapter.java
@@ -0,0 +1,59 @@
+package ryanair.com.resourcesyncexample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import ryanair.com.resourcesyncexample.model.Airport;
+
+public class AirportsAdapter extends RecyclerView.Adapter<AirportsAdapter.ViewHolder> {
+    private List<Airport> mDataSet;
+
+    public AirportsAdapter(List<Airport> dataSet) {
+        mDataSet = dataSet;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v;
+        v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_airport, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Airport airport = mDataSet.get(position);
+
+        holder.airportCode.setText(airport.getCode());
+        holder.airportCountry.setText(airport.getCountry());
+        holder.airportName.setText(airport.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @InjectView(R.id.airport_name)
+        public TextView airportName;
+        @InjectView(R.id.airport_country)
+        public TextView airportCountry;
+        @InjectView(R.id.airport_code)
+        public TextView airportCode;
+        View view;
+
+        public ViewHolder(View v) {
+            super(v);
+            ButterKnife.inject(this, v);
+            view = v;
+        }
+    }
+}
--- mobile/app/src/main/java/ryanair/com/resourcesyncexample/MainActivity.java
+++ mobile/app/src/main/java/ryanair/com/resourcesyncexample/MainActivity.java
@@ -2,20 +2,33 @@ package ryanair.com.resourcesyncexample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.Toast;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import ryanair.com.resourcesyncexample.model.Airport;
 
 
 public class MainActivity extends ActionBarActivity implements StorageManager.AirportsListener {
     private StorageManager mStorageManager;
+    @InjectView(R.id.airports_list)
+    RecyclerView airportsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        airportsList.setLayoutManager(new LinearLayoutManager(this));
+        airportsList.setItemAnimator(new DefaultItemAnimator());
+        airportsList.setAdapter(new AirportsAdapter(new ArrayList<Airport>()));
 
         mStorageManager = new StorageManager(this, this);
         mStorageManager.startLiveQuery();
@@ -23,11 +36,11 @@ public class MainActivity extends ActionBarActivity implements StorageManager.Ai
 
     @Override
     public void onChanged(final List<Airport> airports) {
-        if(airports.size() > 0) {
+        if (airports.size() > 0) {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(getBaseContext(), airports.get(0).getName(), Toast.LENGTH_SHORT).show();
+                    airportsList.setAdapter(new AirportsAdapter(airports));
                 }
             });
         }
